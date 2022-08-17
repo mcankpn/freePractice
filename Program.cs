4b0a68462fb352c3804ef6811df9e6cb75ace02b
Mustafa Can Kapan <mustafacankpn@hotmail.com>
2022-08-17 17:02:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace freePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
